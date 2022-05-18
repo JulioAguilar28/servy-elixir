41ba6402e934408eacd6be3a68424c8194d62624
Julio Aguilar <julio.aguilar@bunsan.io>
2022-05-18 12:45:33 -0500

    initial commit ✨

---
+++ lib/servy/handler.ex
@@ -0,0 +1,142 @@
+defmodule Servy.Handler do
+  @moduledoc "Handle HTTP request"
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  # Where the number is the function arity
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parse, only: [parse: 1]
+
+  def handler(request) do
+    request
+    |> parse()
+    |> rewrite_path()
+    |> log()
+    |> route()
+    |> track()
+    |> format_response()
+  end
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  # def route(%{method: "GET", path: "/about"} = conv) do
+  #   file =
+  #     Path.expand("../../pages", __DIR__)
+  #     |> Path.join("about.html")
+
+  #   case File.read(file) do
+  #     {:ok, contents} ->
+  #       %{conv | status: 200, resp_body: contents}
+
+  #     {:error, :enoent} ->
+  #       %{conv | status: 404, resp_body: "File not found"}
+
+  #     {:error, reason} ->
+  #       %{conv | status: 500, resp_body: "File error: #{reason}"}
+  #   end
+  # end
+
+  def route(%{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(%{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def handle_file({:ok, contents}, conv) do
+    %{conv | status: 200, resp_body: contents}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts(response)
+
+request = """
+GET /foo HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handler(request)
+IO.puts(response)
