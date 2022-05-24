90576cdae4264e7daee1e3cf11f0b56538328c10
Julio Aguilar <julio.aguilar@bunsan.io>
2022-05-23 22:54:05 -0500

    ✨ Finish session 14 Recursion

--- lib/servy/parse.ex
+++ lib/servy/parse.ex
@@ -3,12 +3,24 @@ defmodule Servy.Parse do
 
   def parse(request) do
     [top, params_string] = String.split(request, "\n\n")
-    [request_line | _headers_string] = String.split(top, "\n")
+    [request_line | header_line] = String.split(top, "\n")
     [method, path, _] = String.split(request_line, " ")
-    params = parse_params(params_string)
+    headers = parse_headers(header_line, %{})
+    params = parse_params(headers["Content-Type"], params_string)
 
-    %Conv{method: method, params: params, path: path}
+    %Conv{method: method, params: params, headers: headers, path: path}
   end
 
-  defp parse_params(params), do: params |> String.trim() |> URI.decode_query()
+  defp parse_params("application/x-www-form-urlencoded", params),
+    do: params |> String.trim() |> URI.decode_query()
+
+  defp parse_params(_, _), do: %{}
+
+  defp parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  defp parse_headers([], headers), do: headers
 end
