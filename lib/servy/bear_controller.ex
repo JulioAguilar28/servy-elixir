2c7749162049f51693f53b6aee26185bb1751a6d
Julio Aguilar <julio.aguilar9928@gmail.com>
2022-05-24 23:50:25 -0500

    ✨ Finish lesson 15 Enums part 2

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,16 +1,26 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
+  alias Servy.Bear
 
   def index(conv) do
-    _bears = Wildthings.list_bears()
+    bears =
+      Wildthings.list_bears()
+      |> Enum.filter(fn bear -> Bear.is_grizzly(bear) end)
+      |> Enum.sort(fn bear1, bear2 -> Bear.ord_asc_by_name(bear1, bear2) end)
+      |> Enum.map(fn bear -> bear_item(bear) end)
+      |> Enum.join()
 
-    # TODO: Transform each bear into HTML
+    %{conv | status: 200, resp_body: "<ul>#{bears}</ul>"}
+  end
 
-    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  defp bear_item(bear) do
+    "<li>#{bear.name} - #{bear.type}</li>"
   end
 
   def show(conv, %{"id" => id}) do
-    %{conv | status: 200, resp_body: "Bear #{id}"}
+    bear = Wildthings.get_bear(id)
+
+    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
